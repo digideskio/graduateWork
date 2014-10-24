81236f466b64717cd540642b5f3091fdbe1f6535
faulconbridge <chris@chriswetherill.me>
2014-10-24 17:51:18 -0400

    24 Oct notes

--- Virology/titers.R
+++ Virology/titers.R
@@ -1,3 +1,6 @@
+
+# Original RIO3 and NSV experiments
+# Probs not what we want to ultimately use...
 data <- data.frame(kinase = c(rep("NSV",5),rep("RIO3",5)),
                    vial = c(rep(seq(from = 1, to = 5, by = 1),2)),
                    PFU = c(250000000,200000000,200000000,200000000,100000000,
@@ -18,3 +21,54 @@ barCenters <- barplot(height = mean, names.arg=names,
 segments(barCenters, mean-se*2, barCenters, mean+se*2, lwd=2)
 arrows(barCenters, mean-se*2, barCenters, mean+se*2, lwd=2,
        angle=90, code=3)
+
+# install.packages("devtools")
+library("devtools")
+# install_github("ropensci/plotly")
+library(plotly)
+set_credentials_file("faulconbridge", "5vvex1bdyh")
+# Fill in with your personal username and API key
+# or, use this public demo account
+py <- plotly()
+
+trace1 <- list(
+  x = c("NSV", "RIO3"), 
+  y = c(1.9e+08, 2.6e+08), 
+  name = "Control", 
+  error_y = list(
+    type = "data", 
+    array = c(24494897, 79686887),
+    visible = TRUE
+  ), 
+  type = "bar"
+)
+
+data <- list(trace1)
+layout <- list(barmode = "group")
+response <- py$plotly(data, kwargs=list(layout=layout,
+                                        filename="error-bar-bar",
+                                        fileopt="overwrite"))
+url <- response$url
+
+########################################
+# Titering: round 2!!!
+########################################
+
+trace1 <- list(
+  x = c("NSV", "RIO3", "TGF-alpha", "CSNK2B"), 
+  y = c(1.9e+08, 2.6e+07, 5.72e+07, 1.02e+08), 
+  name = "LVs", 
+  error_y = list(
+    type = "data", 
+    array = c(24494897, 7968688, 18367259, 29016524),
+    visible = TRUE
+  ), 
+  type = "bar"
+)
+
+data <- list(trace1)
+layout <- list(barmode = "group")
+response <- py$plotly(data, kwargs=list(layout=layout,
+                                        filename="validation2",
+                                        fileopt="overwrite"))
+url <- response$url
