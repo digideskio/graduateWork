754a863128a9744fad6524b7c518ebabbb5fb37d
faulconbridge <chris@chriswetherill.me>
2015-01-14 12:04:23 -0500

    Misc commits?

--- Rotations/Garner Lab/MST_pipeline.pl
+++ Rotations/Garner Lab/MST_pipeline.pl
@@ -8,23 +8,16 @@ use Getopt::Long qw(:config no_ignore_case);
 use Thread;
 use Cwd;
 
-my $input;
+# GetOptions("input=s" => \$input)
 
-GetOptions(
-    "input=s" => \$input
-)
 
 my $pwd = "/home/wetherc/";
 my $EXE_DIR = "/home/wetherc/";
 my $trf = "/home/wetherc/software/trf407b.linux64";
-my $reference = "/home/wetherc/reference/hg19.fa";
 my $scripts = "/home/wetherc/scripts/detectMicsUnmapped";
-my $fq1 = "unmapped_1.fastq";
-my $fq2 = "unmapped_2.fastq";
 
 # Specify how qsub script will be named
-my $qsub = "/home/wetherc/unmappedCleaned/qsub.$input.sh";
-my $outdir = "/home/wetherc/unmappedCleaned";
+my $qsub = "/home/wetherc/unmapped/qsub.unmapped.sh";
 my $out;
 
 # Open the qsub script
@@ -33,11 +26,18 @@ open($out, ">$qsub") || die "Could not create a qsub script. Do I have proper wr
 print $out "#!/bin/bash\n";
 print $out "#PBS -W group_list=sfx\n";
 print $out "#PBS -q sfx_q\n";
-print $out "#PBS -N $input\n";
+print $out "#PBS -N unmapped\n";
+print $out "#PBS -r y\n";
 print $out "#PBS -j oe\n";
 print $out "#PBS -l walltime=100:00:00\n";
 print $out "#PBS -l nodes=1:ppn=10\n";
-print $out "#PBS -d /home/wetherc/unmappedCleaned/\n";
+print $out "#PBS -d /home/wetherc/unmapped/\n";
+
+# Define run_cmd function
+print $out "run_cmd () {\n";
+print $out "    echo; echo `date`; echo \$cmd\n";
+print $out "    if !(eval \$cmd); then exit 1; fi;\n";
+print $out "}\n\n";
 
 # Load all requisite modules
 print $out "module load bio/samtools\n";
@@ -46,48 +46,74 @@ print $out "module load bio/bwa\n";
 print $out "module load bio/velvet\n";
 print $out "module load devel/java\n";
 
-# Convert our unmapped sam file into forward and reverse read fastq
-print $out "java -Xmx12g -jar /apps/packages/bio/picard/1.92/bin/SamToFastq.jar VERBOSITY=DEBUG VALIDATION_STRINGENCY=LENIENT INPUT=$input FASTQ=$fq1 SECOND_END_FASTQ=$fq2\n";
+# Merge all sam files
+#
+# These are the remaining reads that were
+# unmapped following the first step of
+# out pipeline where we used bwa
+# to map against hg18
+print $out "cmd=\"samtools merge -nr concatenated/unmapped.concatenated.sam  concatenated/HG*.unmapped.sam\"\n";
+print $out "run_cmd\n\n";
 
-print $out "bwa mem -t 10 $reference $f1 $f2 > $outdir/unmappedCleaned.sam\n";
+# Convert our merged sam file into paired-end
+# fastq files. (Velvet is a memory hog
+# and freaks out if we try to pass
+# the original sam files its way)
+print $out "cmd=\"java -Xmx12g -jar /apps/packages/bio/picard/1.92/bin/SamToFastq.jar INPUT=concatenated/unmapped.concatenated.sam FASTQ=concatenated/unmapped_1.fa SECOND_END_FASTQ=concatenated/unmapped_2.fa\"\n";
+print $out "run_cmd\n\n";
 
-print $out "samtools view -hS $outdir/unmappedCleaned.sam -b | samtools sort - unmappedCleaned\n";
+# Knit together the paired-end fasta
+# files to a single output for generally
+# appeasing Velvet
+print $out "cmd=\"/apps/packages/bio/velvet/current/contrib/shuffleSequences_fasta/shuffleSequences_fasta.pl concatenated/unmapped_1.fa concatenated/unmapped_2.fa unmapped.fa\"\n";
+print $out "run_cmd\n\n";
 
-print $out "samtools index $outdir/unmappedCleaned.bam $outdir/unmappedCleaned.bai\n";
+# Run velvet
+#
+# Sacrifice a goat to your choice diety
+# and pray that there's no seg fault
+#
+# Karthik's script specified a contig length
+# of 71 for this. Not sure why---the docs
+# recommend smaller lengths. But why not?
+print $out "cmd=\"velveth ./velvet 71 -fastq unmapped.fa\"\n";
+print $out "run_cmd\n\n";
 
-print $out "java -Xmx12g -jar /apps/packages/bio/picard/1.92/bin/AddOrReplaceReadGroups.jar I=unmappedCleaned.bam O=unmappedCleaned.rg.bam SORT_ORDER=coordinate RGID=unmappedCleaned RGLB=bwa-mem RGPL=illumina RGSM=unmappedCleaned CREATE_INDEX=true RGPU=RGPU VALIDATION_STRINGENCY=SILENT\n";
+print $out "cmd=\"velvetg ./velvet -scaffolding no -read_trkg yes -amos_file yes -unused_reads yes > ./velvet/velvet_71.log\"\n";
+print $out "run_cmd\n\n";
 
-print $out "java -Xmx12g -jar $scripts/GenomeAnalysisTK.jar -allowPotentiallyMisencodedQuals -T IndelRealigner -R $reference -targetIntervals $scripts/mic.intervals -I $outdir/unmappedCleaned.rg.bam -o unmappedCleaned.GATK.bam\n";
+print $out "cmd=\"$trf ./unmapped_velvet/contigs.fa 2 7 5 80 10 14 6 -h\"\n";
+print $out "run_cmd\n\n";
 
-print $out "perl $scripts/filterUnmappedFromSAM.pl unmappedCleaned.GATK.bam -fa -o unmappedCleaned.fa\n";
+print $out "cmd=\"perl $scripts/makeListFromTRF.pl -r ./unmapped_velvet/contigs.fa -t ./unmapped_velvet/contigs.fa.2.7.5.80.10.14.6.dat -j 0 -o contigs.trf.noJoin.lst\"\n";
+print $out "run_cmd\n\n";
 
-print $out "sh $scripts/process.sh $outdir/unmappedCleaned.fa $outdir/unmappedCleaned_N_filtered.fa\n";
+# print $out "cmd=\"python $scripts/addMotifCount.py ./contigs.trf.noJoin.lst\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "mkdir $outdir/unmapped_velvet/\n";
+# print $out "cmd=\"mv ./contigs.trf.noJoin.lst.tmp contigs.trf.noJoin.lst -f\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "sh $scripts/run_velvet_se.sh 71 $outdir/unmapped_velvet/ $outdir/unmappedCleaned_N_filtered.fa\n";
+# print $out "cmd=\"perl $scripts/report_motifs_ReadCov.pl ./contigs.trf.noJoin.lst ./unmapped_velvet/velvet_asm.afg\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "$trf $outdir/unmapped_velvet/contigs.fa 2 7 5 80 10 14 6 -h\n";
+# print $out "cmd=\"perl $scripts/makeListFromTRF.pl -r ./unmapped_velvet/contigs.fa -t ./unmapped_velvet/contigs.fa.2.7.5.80.10.14.6.dat -j 10 -o contigs.trf.j10.lst\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "perl $scripts/makeListFromTRF.pl -r $outdir/unmapped_velvet/contigs.fa -t $outdir/unmapped_velvet/contigs.fa.2.7.5.80.10.14.6.dat -j 0 -o contigs.trf.noJoin.lst\n";
+# print $out "cmd=\"perl $scripts/addCovReadsNum2MicInContigs.pl -c ./unmapped_velvet/velvet_asm.afg -m contigs.trf.j10.lst -o contigs.trf.j10.readCov.lst\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "python $scripts/addMotifCount.py $outdir/contigs.trf.noJoin.lst\n";
+# print $out "cmd=\"perl $scripts/getMicFlankingFromContigs.pl -c ./unmapped_velvet/contigs.fa -m contigs.trf.j10.readCov.lst -full -o contigs.mic.full.fa\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "mv $outdir/contigs.trf.noJoin.lst.tmp contigs.trf.noJoin.lst -f\n";
+# print $out "cmd=\"blastn -db $scripts/nt_db/nt -evalue 0.001 -outfmt 6 -query contigs.flanking.fa -out contigs.flanking.nt.table\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "perl $scripts/report_motifs_ReadCov.pl $outdir/contigs.trf.noJoin.lst $outdir/unmapped_velvet/velvet_asm.afg\n";
+# print $out "cmd=\"blasatn -db $scripts/human_genome_blast_db/human_genomic_evalue 0.001 -outfmt 6 -query contigs.flanking.fa -out fontigs.flanking.hg19.table\"\n";
+# print $out "run_cmd\n\n";
 
-print $out "perl $scripts/makeListFromTRF.pl -r $outdir/unmapped_velvet/contigs.fa -t $outdir/unmapped_velvet/contigs.fa.2.7.5.80.10.14.6.dat -j 10 -o contigs.trf.j10.lst\n";
-
-print $out "perl $scripts/addCovReadsNum2MicInContigs.pl -c $outdir/unmapped_velvet/velvet_asm.afg -m contigs.trf.j10.lst -o contigs.trf.j10.readCov.lst\n";
-
-print $out "perl $scripts/getMicFlankingFromContigs.pl -c $outdir/unmapped_velvet/contigs.fa -m contigs.trf.j10.readCov.lst -full -o contigs.mic.full.fa\n";
-
-print $out "blastn -db $scripts/nt_db/nt -evalue 0.001 -outfmt 6 -query contigs.flanking.fa -out contigs.flanking.nt.table\n";
-
-print $out "blasatn -db $scripts/human_genome_blast_db/human_genomic_evalue 0.001 -outfmt 6 -query contigs.flanking.fa -out fontigs.flanking.hg19.table\n";
-
-print $out "perl $scripts/getUndetectedMic.pl -f contigs.flanking.fa -m contigs.trf.j10.readCov.lst -b contigs.flanking.hg19.table -nt contigs.flanking.nt.table -o contigs.flanking.txt\n";
+# print $out "cmd=\"perl $scripts/getUndetectedMic.pl -f contigs.flanking.fa -m contigs.trf.j10.readCov.lst -b contigs.flanking.hg19.table -nt contigs.flanking.nt.table -o contigs.flanking.txt\"\n";
+# print $out "run_cmd\n\n";
 
 ######################################################
 
---
+++ Rotations/Garner Lab/README.md
@@ -0,0 +1,134 @@
+Pipeline Documentation
+======================
+
+General File Hierarchy
+----------------------
+
+There are five main directories used in this pipeline:
+
+| - /home
+|   | - wetherc
+|   | - reference
+|   |   | - chromosomal
+|   | - results
+|   | - scripts
+|   |   | - startPipeline
+|   |   | - detectMicsUnmapped
+|   | - software
+|   | - unmapped
+|   |   | - concatenated
+|   |   | - velvet
+|	|	| - unmapped_velvet
+
+./reference contains the human reference genome (hg19) both as the chromosonal .fa files (reference/chromosomal/) and as an indexed reference.
+
+./results is used to temporarily store the full genome sequences as they are downloaded from 1000 Genomes Project and while they are being mapped against the hg19 human reference. Following completion of this, subdirectories are removed and the files containing only unmapped reads (HG*.unmapped.sam) are moved to the ./unmapped directory.
+
+./unmapped contains all unmapped reads against the hg19 human genome (stored in ./unmapped/concatenated). (The concatenated subdirectory is stupidly named. I had things working a bit differently originally and was too lazy to change the directory name and all references to it in various scripts. So it's stupid, but it stayed...) ./unmapped/velvet contains the output of both velveth and velvetg.
+
+Indexing hg19
+-------------
+
+To index the reference, we ran:
+
+```
+	cd ~/wetherc/reference/
+	cat chromosomal/*.fa >> ./hg19.fa
+
+	module load bio/bwa
+
+	bwa index -a bwtsw ./hg19.fa
+```
+
+Step 1: Download genomes
+------------------------
+
+All genomes were first downloaded using wget via the script /home/wetherc/scripts/wget.all.qsub. This serially downloaded all genomes that we wished to analyse from the 1000 Genomes Project. Each genome was downloaded as the original .fastq.gz files provided by the 1000 Genomes Project into its own directory at ~/wetherc/results/HG* with HG* being the sample name for that individual's genome.
+
+Step 2: Mapping against hg19
+----------------------------
+
+For each genome downloaded, we ran `/home/wetherc/scripts/startPipeline/make_script.pl` using the syntax:
+
+```
+	perl /home/wetherc/scrips/startPipeline/make_script.pl --input HG*
+```
+
+where again HG* was the complete genome name (e.g., HG04035). This script then created and submitted a qsub job that:
+
+  - unzipped all fastq files in the specified directory;
+  - concatenated all of the forward and backward reads, respectively, into HG*.pairs_1.fastq and HG*.pairs_2.fastq
+  - mapped the paired reads against hg19 by `bwa mem -aY -t 5 -P -v 3 -U 25`
+  - sorted alignments of the resultant sam file by leftmost coordinates
+  - removed PCR duplicates by `samtools rmdup`
+  - indexed sorted alignment for fast random access
+  - used Picard's AddOrReplaceReadGroups to replace all read groups in the INPUT file with a single new read group and assigns all reads to this read group in the OUTPUT BAM
+  - created sam file containing only unmapped reads from the original genome via `samtools view -u -f 4`
+  - moved resultant file (HG*.unmapped.sam) to /home/wetherc/unmapped/concatenated
+
+Step 2.5: Count unmapped reads in each genome
+---------------------------------------------
+
+To construct a count of the number of unmapped reads against the hg19 human reference for each genome parsed, we run `sh /home/wetherc/unmapped/concatenated/countUnmapped.sh` which executes:
+
+```
+	#!/bin/bash
+
+	module load bio/samtools
+
+	for file in /home/wetherc/unmapped/concatenated/*.unmapped.sam
+	do
+		printf "${file:36:7}\t" >> output.txt
+		samtools view -c -fox4 $file >> output.txt
+	done
+```
+
+This constructs an output.txt file that on each line contains (tab separated) the genome name and the number of unmapped reads against hg19 for that genome.
+
+Step 3: Construct de novo contigs and blast
+-------------------------------------------
+
+To construct de novo contigs from our unmapped reads and blast them against known sources, we run `perl /home/wetherc/scripts/detectMicsUnmapped/MST.pl`.
+
+This begins by first mergins all of our HG*.unmapped.sam files (from step 2) into a single sorted sam file using:
+
+```
+	samtools merge -nr unmapped.concatenated.sam  /home/wetherc/unmapped/concatenated/HG*.unmapped.sam
+```
+
+This merged sam file must then by converted into fasta format. (Technically, this doesn't *have* to be the case, but Velvet is a memory hog and if we try to pass it a sam file, chances are very good that we will then have a seg fault. That's just bad times for everyone.)
+
+To do this, our script runs:
+
+```
+	# Convert our sam file into two paired-read .fa files
+	java -Xmx12g -jar /apps/packages/bio/picard/1.92/bin/SamToFastq.jar INPUT=unmapped.concatenated.sam FASTQ=unmapped_1.fa SECOND_END_FASTQ=unmapped_2.fa
+
+	# Stitch those two paired-read files into a single
+	# output that we can pass to Velvet
+	/apps/packages/bio/velvet/current/contrib/shuffleSequences_fasta/shuffleSequences_fasta.pl unmapped_1.fa unmapped_2.fa unmapped.fa
+```
+
+- - - - - -
+NOTE:
+
+Karthik's original pipeline at this point passed a fasta file through:
+
+```
+	perl -e 'my ($name,$seq,$s,$e,$k,$l);while(<>){if(/^>/){$name = $_;}
+
+	else{ chomp;$seq=$_;$l=length($seq);$s=0;$e=$l;$k=0;
+	 for($i=0;$i<$l;$i++){ $c=substr($seq, $i, 1); if($c eq "N"){$k=0;$s=$i+1;}else{$k++; if($k==10){last;}}}
+	 for($i=$l-1,$k=0;$i>$s;$i--){ $c=substr($seq, $i, 1); if($c eq "N"){$e=$i; $k=0;}else{$k++; if($k==10){last;}}}
+	 if($s!=0 || $e != $l){ $seq=substr($seq,$s,$e-$s);}
+	 if(length($seq)>50){ print "$name$seq\n";}
+	 }
+	}' $infile > $outfile
+```
+
+That was indecipherable. I elected to not use it. If that's an issue, it should probably be reinserted into the MST.pl script. This function is stored in `~/home/wetherc/scripts/detectMicsUnmapped/process.sh`.
+- - - - - -
+
+The script then runs `velveth` on the fasta input (unmapped.fa). Karthik's script specified a hash length of 71. Not sure why; according to the docs anything above 31 will generally be automatically reduced.
+
+We then call `velvetg` for de Bruijn graph construction, error removal and repeat resolution.
